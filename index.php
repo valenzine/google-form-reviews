023a54be4c1849fe64f2f24f31dd088391a79702
Valentín Muro <valentinmuro@gmail.com>
2025-01-09 14:00:03 +0100

    Update version to 0.1.3 and append version query string to stylesheet link

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if (isset($_GET['ajax'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($websiteTitle); ?></title>
     <meta name="description" content="<?php echo htmlspecialchars($websiteDescription); ?>">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo $version; ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
-$version = '0.1.2';
+$version = '0.1.3';
 ?>
